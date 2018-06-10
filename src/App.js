181853e4140aabc46c3903db677130f96fb24bce
pressmore <56924663@qq.com>
2018-06-10 11:00:41 +0800

    create first react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="root">
+        <p>
+          熊某人还是挺诚实的... Hello React
+        </p>
+        </div>
+    );
+  }
+}
+
+export default App;
